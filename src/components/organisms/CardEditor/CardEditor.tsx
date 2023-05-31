d6233c2bfcd1e6004d867c38a700b352677f4591
Amir <amirfazelinia@gmail.com>
2023-05-31 11:08:40 +0330

    connecting list and card editor to context

--- src/components/organisms/CardEditor/CardEditor.tsx
+++ src/components/organisms/CardEditor/CardEditor.tsx
@@ -1,6 +1,7 @@
 import styles from './CardEditor.module.css';
 import React, { useState } from 'react';
 import { EditButtons, TextField } from '../../../components';
+import { ActionTypes } from '../../../interfaces';
 import { useBoardContext } from '../../../hooks';
 
 interface CardEditorProps {
@@ -11,7 +12,7 @@ interface CardEditorProps {
 }
 
 const CardEditor = ({ listIndex, cardIndex, isEditMode, onFinish }: CardEditorProps) => {
-  const { state } = useBoardContext();
+  const { state, dispatch } = useBoardContext();
   const { lists } = state;
   const [title, setTitle] = useState<string>(lists?.[listIndex]?.cards?.[cardIndex]?.title || '');
 
@@ -21,16 +22,36 @@ const CardEditor = ({ listIndex, cardIndex, isEditMode, onFinish }: CardEditorPr
     onFinish?.();
   };
 
+  const saveCard = () => {
+    finishEdit();
+    if (isEditMode) {
+      dispatch({
+        type: ActionTypes.EDIT_CARD,
+        payload: { listIndex, index: cardIndex, title },
+      });
+    } else {
+      dispatch({
+        type: ActionTypes.ADD_CARD,
+        payload: { listIndex, title },
+      });
+    }
+  };
+
+  const deleteCard = () => {
+    if (window.confirm('Are you sure to delete this card?')) {
+      dispatch({
+        type: ActionTypes.DELETE_CARD,
+        payload: { listIndex, index: cardIndex },
+      });
+    }
+  };
+
   return (
     <>
       <div className={styles.editing}>
         <TextField placeholder={'Enter card title...'} value={title} onChange={changeTitle} />
       </div>
-      <EditButtons
-        onSave={() => alert('save card')}
-        onDelete={isEditMode ? () => alert('delete card') : undefined}
-        onCancel={finishEdit}
-      />
+      <EditButtons onSave={saveCard} onDelete={isEditMode ? deleteCard : undefined} onCancel={finishEdit} />
     </>
   );
 };
--- src/components/organisms/ListEditor/ListEditor.tsx
+++ src/components/organisms/ListEditor/ListEditor.tsx
@@ -2,6 +2,7 @@ import styles from './ListEditor.module.css';
 import React, { useState } from 'react';
 import { useBoardContext } from '../../../hooks';
 import { EditButtons, ListControls, TextField } from '../../../components';
+import { ActionTypes } from '../../../interfaces';
 
 interface ListEditorProps {
   index?: number;
@@ -10,23 +11,43 @@ interface ListEditorProps {
 }
 
 const ListEditor = ({ index, isEditMode, onFinish }: ListEditorProps) => {
-  const { state } = useBoardContext();
+  const { state, dispatch } = useBoardContext();
   const { lists } = state;
   const [title, setTitle] = useState<string>(lists?.[index]?.title || '');
 
   const changeTitle = (e: React.ChangeEvent<HTMLTextAreaElement>) => setTitle(e.target.value);
 
+  const deleteList = () => {
+    if (window.confirm('Are you sure to delete this list?')) {
+      dispatch({
+        type: ActionTypes.DELETE_LIST,
+        payload: { index },
+      });
+    }
+  };
+
+  const saveList = () => {
+    onFinish?.();
+    if (isEditMode) {
+      dispatch({
+        type: ActionTypes.EDIT_LIST_TITLE,
+        payload: { index, title },
+      });
+    } else {
+      dispatch({
+        type: ActionTypes.ADD_LIST,
+        payload: { title },
+      });
+    }
+  };
+
   return (
     <>
       <div className={styles.editing}>
         <TextField placeholder='Enter list title...' value={title} onChange={changeTitle} />
-        {isEditMode && <ListControls onDelete={() => alert('delete list')} />}
+        {isEditMode && <ListControls onDelete={deleteList} />}
       </div>
-      <EditButtons
-        onSave={() => alert('save list')}
-        onCancel={onFinish}
-        onDelete={isEditMode ? () => alert('delete list') : undefined}
-      />
+      <EditButtons onSave={saveList} onCancel={onFinish} onDelete={isEditMode ? deleteList : undefined} />
     </>
   );
 };
